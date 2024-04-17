85d0a6a1592aca83e3bb96705311e7c86072f56c
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-04-17 21:20:56 +0200

    calculating bmi and displaing bmi resualts

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 let formElement = document.querySelector(".js-form");
 let weightInputElement = document.querySelector(".js-weightField");
 let heightInputElement = document.querySelector(".js-heightField");
-let calculateBmiElement = document.querySelector(".js-calculateBMIButton");
-let bmiResualtsElement = document.querySelector(".js-bmiResualts");
+let resualtElemet = document.querySelector(".js-bmiResualts");
+
+formElement.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let weight = weightInputElement.value;
+    let height = heightInputElement.value;
+    let bmi = weight / ((height / 100) ** 2);
+
+    resualtElemet.innerText = bmi.toFixed(2);
+
+    formElement.reset();
+    weightInputElement.focus();
+});
