bb5796c4eff96d1ccd9bfa96a365b2eaabb4cab8
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-04-17 20:41:51 +0200

    Created variables for form's elements

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let formElement = document.querySelector(".js-form");
+let weightInputElement = document.querySelector(".js-weightField");
+let heightInputElement = document.querySelector(".js-heightField");
+let calculateBmiElement = document.querySelector(".js-calculateBMIButton");
+let bmiResualtsElement = document.querySelector(".js-bmiResualts");
